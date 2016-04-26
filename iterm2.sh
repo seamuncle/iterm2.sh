e6c9d1f01070984b38aeba2685bd67df69763683
Matt Hilliard <matt.hilliard@gmail.com>
2016-04-26 15:09:02 -0600

    Made functions work in tandem with /bin/sh
    Oldskool bourne shell doesn't like dashes in function names.  Replaced these with underscores.

--- iterm2.sh
+++ iterm2.sh
@@ -7,15 +7,15 @@
 
 if [ -n $ITERM_SESSION_ID ]; then
 
-    tab-color() {
+    tab_color() {
         echo -ne "\033]6;1;bg;red;brightness;$1\a";
         echo -ne "\033]6;1;bg;green;brightness;$2\a";
         echo -ne "\033]6;1;bg;blue;brightness;$3\a";
     }
     # public
-    export -f tab-color
+    export -f tab_color
     
-    bg-color() {
+    bg_color() {
         # divide colours by 4 to darken; and translate to hex
         R=$( printf "%02x" `expr $1 / 4` )
         G=$( printf "%02x" `expr $2 / 4` )
@@ -25,22 +25,22 @@ if [ -n $ITERM_SESSION_ID ]; then
         echo -ne "\033]Ph$R$G$B\033\\"
     }
     # public
-    export -f bg-color
+    export -f bg_color
     
-    change-color() {
-	tab-color $1 $2 $3
-	bg-color $1 $2 $3
+    change_color() {
+	tab_color $1 $2 $3
+	bg_color $1 $2 $3
     }
     # public
-    export -f change-color
+    export -f change_color
 
-    tab-red() { tab-color 255 0 0; }
+    tab_red() { tab_color 255 0 0; }
 
-    tab-green() { tab-color 0 255 0; }
+    tab_green() { tab_color 0 255 0; }
 	
-    tab-blue() { tab-color 0 0 255; }
+    tab_blue() { tab_color 0 0 255; }
     
-    tab-reset() { echo -ne "\033]6;1;bg;*;default\a"; }
+    tab_reset() { echo -ne "\033]6;1;bg;*;default\a"; }
     # public
     export -f tab-reset
 
@@ -52,12 +52,12 @@ if [ -n $ITERM_SESSION_ID ]; then
         if [[ $1 =~ $ssh_pattern ]]; then
     	    prod_pattern='prod'
             if [[ $1 =~ $prod_pattern ]]; then
-               change-color 255 160 160
+               change_color 255 160 160
             else
-               change-color 160 255 160
+               change_color 160 255 160
             fi
         else
-  	    change-color 160 160 255
+  	    change_color 160 160 255
         fi
     }
     precmd_functions+=(iterm2_tab_precmd)
